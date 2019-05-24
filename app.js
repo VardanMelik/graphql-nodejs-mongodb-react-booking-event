b31acdaac386c6fc0efcdd0be078055ef0153f31
eyemotion <eyemotiongamestudio@gmail.com>
2019-05-24 11:52:44 +0400

    'update'

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express()
 
 app.use(bodyParser.json())
 
+// MiddleWare Function
+app.get('/', (req, res, next) => {
+    res.send('Hello ')
+})
+
 app.listen(3000, () => {
     console.log('Server is running')
 })
